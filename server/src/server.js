869bf2c27e0be5256fe82b35151922cfc3ebe915
Maryen <voujum@gmail.com>
2023-07-05 16:43:18 +0200

    File arhitecture, client and server setup

---
+++ server/src/server.js
@@ -0,0 +1,11 @@
+// server http
+const http = require('http');
+const app = require('./app');
+
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log('Listening on a port:', PORT);
+});
