7c3b8a94e7b2767f47bb6152129dcee6722f64c4
Maryen <voujum@gmail.com>
2023-07-09 09:43:29 +0200

    Paginate API functionality

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -110,7 +110,11 @@ async function getLatestFlightNumber() {
 }
 
 async function getAllLaunches(skip, limit) {
-  return await launches.find({}, { _id: 0, __v: 0 }).skip(skip).limit(limit);
+  return await launches
+    .find({}, { _id: 0, __v: 0 })
+    .sort({ flightNumber: 1 })
+    .skip(skip)
+    .limit(limit);
 }
 
 async function scheduleNewLaunch(launch) {
